50537de63f5fdee297c8a3a735f31231467d4498
Saurabh Kashyap <saurabhkashyap0001@gmail.com>
2023-08-20 00:50:37 +0530

    Improve Code Example Practice

--- src/FindTheLargestNumber.java
+++ src/FindTheLargestNumber.java
@@ -5,21 +5,26 @@ public class FindTheLargestNumber {
     public static void main(String[] args){
         System.out.println("Enter How much your Array is: ");
         int Numb = new Scanner(System.in).nextInt();
+        System.out.printf("Enter %d Uniques Number: ",Numb);
         Scanner scanner = new Scanner(System.in);
 
         int[] numbers = new int[Numb];
+        int largest = numbers[0];// 0
 
-        System.out.println("Enter 5 numbers:");
         for (int i = 0; i < Numb; i++) {
             numbers[i] = scanner.nextInt();
         }
 
-        int largest = numbers[0];// 0
-        for (int i = 1; i < Numb; i++) {
-            if (numbers[i] > largest) {
-                largest = numbers[i];
+            for(int i:numbers){
+                if(i>largest)
+                    largest = i;
             }
-        }
+
+//        for (int i = 1; i < Numb; i++) {
+//            if (numbers[i] > largest) {
+//                largest = numbers[i];
+//            }
+//        }
 
         System.out.println("The largest number is: " + largest);
 
