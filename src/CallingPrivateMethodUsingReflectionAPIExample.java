c3589c2c4da823af8cd27afa91a6cd4b6c85c478
Saurabh Kashyap <saurabhkashyap0001@gmail.com>
2023-08-20 20:46:57 +0530

    Private Method for calling Reflection API  Examples

---
+++ src/CallingPrivateMethodUsingReflectionAPIExample.java
@@ -0,0 +1,25 @@
+import java.lang.reflect.Method;
+
+public class CallingPrivateMethodUsingReflectionAPIExample {
+
+    public static void main(String[] args) throws Exception{
+//        Class c = Class.forName("com.JavaPrep.Test");
+//        Test t =(Test) c.newInstance();
+//
+//        Method m= c.getDeclaredMethod("show",null);
+//        m.setAccessible(true);
+//        m.invoke(t,null);
+
+//        Class c1 = Class.forName("com.JavaPrep.dds");
+//        System.out.println(c1.getSuperclass());
+
+    }
+}
+
+class sdd {
+
+}
+
+class dds extends sdd {
+
+}
