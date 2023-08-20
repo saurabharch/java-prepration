443baac25a7f52525d77a21bed872159636f4e59
Saurabh Kashyap <saurabhkashyap0001@gmail.com>
2023-08-20 12:00:04 +0530

    QRCode Generation Example Practice

---
+++ src/QRCodeGeneratorExample.java
@@ -0,0 +1,19 @@
+import net.glxn.qrgen.QRCode;
+import net.glxn.qrgen.image.ImageType;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class QRCodeGeneratorExample {
+
+    public static void main(String[] args) throws Exception {
+        String details = "Saurabh Kashyap : full stack Developer";
+        ByteArrayOutputStream out = QRCode.from(details).to(ImageType.PNG).withSize(460,460).stream(); // ByteArrayOutputStream only takes stream
+        File f = new  File("D:\\SpringTest_Source\\JavaPrep\\lib\\MyQRCode.png");
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write(out.toByteArray());
+        fos.flush();
+
+    }
+}
