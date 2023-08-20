5eb6eb59b83ae9f8e2b9203ccbeb5293d6a194f8
Saurabh Kashyap <saurabhkashyap0001@gmail.com>
2023-08-20 12:23:46 +0530

    File Handle with File I/O, Data I/O  Example Update

--- src/FileHandlingExample.java
+++ src/FileHandlingExample.java
@@ -4,11 +4,12 @@ public class FileHandlingExample {
 
     public static void main(String[] args) throws Exception {
 
-        FileOutputStream fos = new FileOutputStream("demo.txt");
+        File f = new File("demo.text");
+        FileOutputStream fos = new FileOutputStream(f);
         DataOutputStream dos = new DataOutputStream(fos);
         dos.writeUTF("Saurabh Kashyap");
 
-        FileInputStream fis = new FileInputStream("demo.txt");
+        FileInputStream fis = new FileInputStream(f);
         DataInputStream dis = new DataInputStream(fis);
         String rs = dis.readUTF(dis);
         System.out.println("Result: "+rs);
