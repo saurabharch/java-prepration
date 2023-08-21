aefadc5cddd0deeafdca79339471fe31e478e820
saurabharch <saurabhkashyap0001@gmail.com>
2023-08-21 18:28:26 +0530

    Update the code and fix the Show current date/time/zone examples

--- src/useCurrentTimeMillisCalculateExecutionTime.java
+++ src/useCurrentTimeMillisCalculateExecutionTime.java
@@ -1,19 +1,26 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 public class useCurrentTimeMillisCalculateExecutionTime {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         long start = System.currentTimeMillis();
-        // create an instance of the SimpleDateFormat class for modifying the date object
 
-//        DateFormat obj = new SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z");
-//        // we create instance of the Date and pass milliseconds to the constructor
-//        Date res = new Date(start);
-//        // now we format the res by using SimpleDateFormat
-//        System.out.println(obj.format("Current data Time : "+res));
+        // create an instance of the SimpleDateFormat class for modifying the date object
+        //convert milliseconds into Date by creating an object of the date class and passing milliseconds to the constructor
+        Date res = new Date(start);
+        DateFormat obj = new SimpleDateFormat("dd MM yyyy HH:mm:ss:SSS Z");
+
+        Calendar calendar = Calendar.getInstance();
+        // use setTimeInMillis() method od the calendar class to set time
+        calendar.setTimeInMillis(start);
+        // after formatting the date into dd:MM:yy:HH:mm:ss
+        //copying data of one date into another one
+        Date currentdate = new Date();
+        System.out.println("Today Date Time/Zone: " + obj.format(currentdate));
         ArrayList<Integer> num = new ArrayList<>();
 
         for(int a=0;a<=1000000;a++){
