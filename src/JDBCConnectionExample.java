f8aa31555f4277b97c5778d65bab42b1779b6303
Saurabh Kashyap <saurabhkashyap0001@gmail.com>
2023-08-22 14:17:10 +0530

    JDBC Connection Examples

--- src/JDBCConnectionExample.java
+++ src/JDBCConnectionExample.java
@@ -1,18 +1,29 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.*;
+import java.util.Properties;
+
 public class JDBCConnectionExample {
 
     public static void main(String[] args) throws Exception{
-        String url = "jdbc:mysql://localhost:3306/database";
-        String uname = "root";
+
+//        Properties p = new Properties();
+//        InputStream Ips = new FileInputStream("dataConfig.properties");
+//        String ul1 = p.getProperty("url");
+//        System.out.println("Result: "+ul1);
+        String url = "jdbc:mysql://localhost:3306/mysqldb";
+        String uname = "user";
         String pass ="password";
-        String query = "select username from student where user_id=2";
+        String query = "select * from Student ";
 
         Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection(url,uname,pass);
         Statement st = con.createStatement();
         ResultSet rt = st.executeQuery(query);
         rt.next();
-        String result = rt.getString("username");
+        String result = rt.getString("name");
         System.out.println(result);
         rt.close();
         con.close();
