87823229c8435bab4baa0ae98e21775b40cf318b
Saurabh Kashyap <saurabhkashyap0001@gmail.com>
2023-08-22 18:51:43 +0530

    Stream Map Reduce Examples

--- src/StreamMapReduceMethodsExample.java
+++ src/StreamMapReduceMethodsExample.java
@@ -7,9 +7,26 @@ public class StreamMapReduceMethodsExample {
     public static void main(String[] args) {
         List<Integer> value = Arrays.asList(2,3,4,5,6,7,8,9);
         long start = System.currentTimeMillis();
+
         System.out.println(value.stream().map(i -> i*2).reduce(0,(c,e)-> c+e));
         long End = System.currentTimeMillis();
         long Result= End - start;
         System.out.println(Result+"ms");
+        long start1 = System.currentTimeMillis();
+        System.out.println(value.stream().map(i -> i*2).reduce(0,Integer::sum));
+        long End1 = System.currentTimeMillis();
+        long Result1= End - start;
+        System.out.println(Result1+"ms");
+
+        /*
+        RESULT PERFORMANCE:
+        Method: 1
+            88
+            2ms
+         Method: 2
+            88
+            2ms
+
+         */
     }
 }
